557989f9a6f1e04d3f8efbcfe98867736f9ef468
Vladislava Soldatenko <vladislavasoldatenko@yandex.ru>
2025-01-16 17:16:48 +0300

    Допзадание

--- lab6.py
+++ lab6.py
@@ -8,28 +8,60 @@ class OpticalDisc:
         bytes_per_rotation = self.capacity / (self.rotations * 60)
         return bytes_per_rotation * 20
 
+    def __add__(self, other):
+        if isinstance(other, self.__class__):
+            new_capacity = self.capacity + other.capacity
+            return self.__class__(
+                capacity=new_capacity,
+                rotations=self.rotations,  
+                laser_thickness_nm=self.laser_thickness_nm,  
+                **self._get_additional_args()  
+            )
+        else:
+            raise TypeError(f"Нельзя сложить {self.__class__.__name__} с {other.__class__.__name__}")
+
+    def _get_additional_args(self):
+        return {}
+
+
 class CD(OpticalDisc):
-    def __init__(self, capacity, rotations, laser_thickness_nm, cd_type):
+    def __init__(self, capacity, rotations, laser_thickness_nm, cd_type="Unknown"):
         super().__init__(capacity, rotations, laser_thickness_nm)
-        self.cd_type = cd_type 
+        self.cd_type = cd_type
 
     def average_rotation_time(self):
         return 60 / self.rotations 
 
+    def _get_additional_args(self):
+        return {"cd_type": self.cd_type}
+
+
 class DVD(OpticalDisc):
-    def __init__(self, capacity, rotations, laser_thickness_nm, dvd_layer):
+    def __init__(self, capacity, rotations, laser_thickness_nm, dvd_layer=1):
         super().__init__(capacity, rotations, laser_thickness_nm)
         self.dvd_layer = dvd_layer  
 
     def average_rotation_time(self):
-        return 60 / self.rotations 
+        return 60 / self.rotations  
+
+    def _get_additional_args(self):
+        return {"dvd_layer": self.dvd_layer}
+
 
 if __name__ == "__main__":
-    cd = CD(capacity=700 * 1024 * 1024, rotations=200, laser_thickness_nm=780, cd_type="CD-R")
-    dvd = DVD(capacity=4700 * 1024 * 1024, rotations=1200, laser_thickness_nm=650, dvd_layer=2)
+    cd1 = CD(capacity=700 * 1024 * 1024, rotations=200, laser_thickness_nm=780, cd_type="CD-R")
+    cd2 = CD(capacity=800 * 1024 * 1024, rotations=200, laser_thickness_nm=780, cd_type="CD-RW")
 
-    print(f"CD: Байт за 20 секунд: {cd.bytes_per_20_seconds()}")
-    print(f"CD: Среднее время одного оборота: {cd.average_rotation_time()} секунд")
+    dvd1 = DVD(capacity=4700 * 1024 * 1024, rotations=1200, laser_thickness_nm=650, dvd_layer=2)
+    dvd2 = DVD(capacity=8500 * 1024 * 1024, rotations=1200, laser_thickness_nm=650, dvd_layer=2)
 
-    print(f"DVD: Байт за 20 секунд: {dvd.bytes_per_20_seconds()}")
-    print(f"DVD: Среднее время одного оборота: {dvd.average_rotation_time()} секунд")
+    cd_sum = cd1 + cd2
+    print(f"CD1 + CD2: Ёмкость = {cd_sum.capacity / (1024 * 1024)} МБ, Тип = {cd_sum.cd_type}")
+
+    dvd_sum = dvd1 + dvd2
+    print(f"DVD1 + DVD2: Ёмкость = {dvd_sum.capacity / (1024 * 1024)} МБ, Слоёв = {dvd_sum.dvd_layer}")
+
+    try:
+        invalid_sum = cd1 + dvd1
+    except TypeError as e:
+        print(e)
